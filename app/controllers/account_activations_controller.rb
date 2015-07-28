e52ed1aac05ac64ae3fbfca16167b862e9e841eb
Darren Chen <yandachen2001@gmail.com>
2015-07-28 00:04:03 -0400

    Add password resets & email configuration

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,6 +1,8 @@
 class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
+    # activation token is as params[:id] because of the url:
+    # http://www.example.com/account_activations/q5lt38hQDc_959PVoo6b7A/edit
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -6,12 +6,13 @@ class UserMailerPreview < ActionMailer::Preview
   	user = User.first
   	user.activation_token = User.new_token # needed by account_activation.text.erb in views
     # the activation in user.rb doesn't work coz it only fires up when user sign up.
-    UserMailer.account_activation(user)
+    UserMailer.account_activation(user) # defined in user_mailer.rb
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
-
 end
