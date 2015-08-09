39c729cc4b5c7f41dbc6c91868476264849b0331
yyddcc <yandachen2001@gmail.com>
2015-08-08 22:17:41 -0400

    lineup indentation

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
 
   root             			'static_pages#home'
-  get		'help'		=>	'static_pages#help'
-  get		'about'   	=>	'static_pages#about'
-  get		'contact' 	=>	'static_pages#contact'
-  get		'signup'  	=>	'users#new'
-  get		'login'		=>	'sessions#new'
+  get		  'help'		=>	'static_pages#help'
+  get		  'about'   =>	'static_pages#about'
+  get		  'contact' =>	'static_pages#contact'
+  get		  'signup'  =>	'users#new'
+  get		  'login'		=>	'sessions#new'
   post		'login'		=>	'sessions#create'
   delete	'logout'	=>	'sessions#destroy'
   resources :users do
